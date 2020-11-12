13822a25f1083c416ecc33f060888e399c325d4c
Robert Aspirot <raspirot@genetec.com>
2020-11-11 23:56:12 -0500

    update searchbox

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 import './search-box.styles.css';
 
-export const SearchBox = ({ onSearchChange }) => (
-  <input 
-    className='search'
-    type='search' 
+export const SearchBox = (props) => (
+  <input
+    className='search-box'
+    type='search'
     placeholder='search monsters'
-    onChange={onSearchChange}
+    onChange={props.onSearchChange}
   />
-)
+);
